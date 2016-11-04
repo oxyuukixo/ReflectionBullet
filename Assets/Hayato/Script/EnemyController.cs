11388297d0e45ea61865b967470c2036a197fe85
hayato3546 <hayato55641@gmail.com>
2016-11-04 11:33:29 +0900

    no message

---
+++ Assets/Hayato/Script/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform target; // 追いかける対象
+    public float rotMax; // 回転速度
+    public float speed = 1; // 移動スピード
+
+    void Update()
+    {
+        // ターゲット方向のベクトルを求める
+        Vector3 vec = target.position - transform.position;
+
+        // ターゲットの方向を向く
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(vec.x, 0, vec.z)), rotMax);
+        // 正面方向に移動
+        transform.Translate(Vector3.forward * speed); 
+    }
+}
