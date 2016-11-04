6a7fe6a78d418f67e5be02d2e5aa20f42f87e447
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-11-04 10:11:55 +0900

    no message

---
+++ Assets/Yu-ki/Scripts/Bullet.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public enum BulletType
+    {
+        Normal,
+        Penetration,
+        Diffusion,
+        Exploding,
+        Division
+    }
+
+    public BulletType m_Type;
+
+    bool IsDestroy = false;
+
+    public int HitHP;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(IsDestroy)
+        {
+            GameObject BulletObj;
+
+            for (int i = 1; i <= 2; i++)
+            {
+                BulletObj = Instantiate(gameObject);
+
+                BulletObj.transform.position = transform.position;
+
+                BulletObj.GetComponent<Rigidbody2D>().velocity = Quaternion.Euler(0, 0, 30 * Mathf.Pow(-1,i)) * GetComponent<Rigidbody2D>().velocity;
+
+                BulletObj.GetComponent<Bullet>().HitHP = HitHP;
+
+                Debug.Log(BulletObj.GetComponent<Rigidbody2D>().velocity);
+            }
+
+            Destroy(gameObject);
+        }
+
+	}
+
+    void OnCollisionEnter2D(Collision2D Hit)
+    {
+        HitHP--;
+
+        if(m_Type == BulletType.Division && HitHP > 0)
+        {
+            IsDestroy = true;
+        }
+        else if(HitHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Yu-ki/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    RaycastHit2D ray;
+
+    Vector2 FireDir = new Vector2(1,0);
+
+    public GameObject Bullet;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float XAxis = Input.GetAxis("Horizontal");
+        float YAxis = Input.GetAxis("Vertical");
+
+        if(XAxis != 0f)
+        {
+            if (Mathf.Abs(XAxis) >= 1)
+            {
+                FireDir.x = Mathf.Abs(FireDir.x) * Mathf.Sign(XAxis);
+            }
+        }
+
+        if (YAxis != 0f)
+        {
+            FireDir = Quaternion.Euler(0, 0, YAxis * Mathf.Sign(FireDir.x)) * FireDir;
+        }
+
+        if(Input.GetKeyDown("joystick button 0"))
+        {
+            GameObject BulletObj = Instantiate(Bullet);
+
+            BulletObj.transform.position = transform.position;
+
+            BulletObj.GetComponent<Rigidbody2D>().velocity = FireDir * 20;
+        }
+
+        ray = Physics2D.Raycast(transform.position, -Vector2.up);
+
+        Debug.DrawRay(transform.position,FireDir * 5000, new Color(255,0,0));
+    }
+}
