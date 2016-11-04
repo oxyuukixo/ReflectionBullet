5eb8b539771f34278d2feca4b7af7433b2392f1d
hayato3546 <hayato55641@gmail.com>
2016-11-04 15:57:08 +0900

    テストマップとか敵の突撃スクリプト

--- Assets/Hayato/Script/EnemyController.cs
+++ Assets/Hayato/Script/EnemyController.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-    public Transform target; // 追いかける対象
-    public float rotMax; // 回転速度
-    public float speed = 1; // 移動スピード
+    public GameObject target;
+    Vector3 prev; // １フレーム前の座標保持
+    public float speed = 0.1f; // 移動量
+    public float moveForce = 10.0f; // 移動量(Rigidbody用)
 
+    void Start()
+    {
+        prev = this.gameObject.transform.localPosition;
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        // ターゲット方向のベクトルを求める
-        Vector3 vec = target.position - transform.position;
+        Vector2 enemyPos = this.gameObject.transform.localPosition;
+        float vx = target.transform.position.x - enemyPos.x;
+        //float vy = target.transform.position.y - enemyPos.y;
+        float dx, dy, radian, value;
 
-        // ターゲットの方向を向く
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(vec.x, 0, vec.z)), rotMax);
-        // 正面方向に移動
-        transform.Translate(Vector3.forward * speed); 
+        /**
+    　　　　 * ターゲットとのラジアン/ベクトル
+    　　　　 */
+        // Mathf.Atan2を使う場合
+        radian = Mathf.Atan2(0, vx); // ２つの座標から角度を計算
+        dx = Mathf.Cos(radian) * speed; // Sin,Cosを使用してその方向へ移動
+        //dy = Mathf.Sin(radian) * speed;
+
+        // Mathf.Atan2を使わない場合
+        //value = Mathf.Sqrt( (vx * vx) + (vy * vy) ); // ２つの座標からベクトル計算
+        //dx = (vx / value) * speed; // valueで割って正規化
+        //dy = (vy / value) * speed;
+
+        // 移動制御
+        if (Mathf.Abs(vx) < 0.1f)
+        {
+            dx = 0f;
+        }
+        //if (Mathf.Abs(vy) < 0.1f)
+        //{
+        //    dy = 0f;
+        //}
+
+        // 移動を反映
+        enemyPos.x += dx;
+        //enemyPos.y += dy;
+
+        this.gameObject.transform.localPosition = enemyPos;
     }
 }
