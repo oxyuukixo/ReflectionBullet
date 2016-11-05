1f8cf9890b76f7ced546475842d3c3f84b1567f1
hayato3546 <hayato55641@gmail.com>
2016-11-05 15:45:58 +0900

    弾撃つ敵追加、突撃敵移動修正、マップ当たり判定のズレ修正etc

---
+++ Assets/Hayato/Script/B_EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_EnemyController : MonoBehaviour {
+
+    public int speed;
+
+    public GameObject bullet;
+
+    public GameObject target;
+
+    public GameObject shootposition;
+
+
+    // Use this for initialization
+    IEnumerator Start () {
+
+        shootposition = gameObject.transform.FindChild("BulletPosition").gameObject;
+
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置で発射
+            Instantiate(bullet, transform.position, shootposition.transform.rotation);
+            // 1秒待つ
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 scale = transform.localScale;
+        float dx = (target.transform.position.x - transform.position.x);
+
+        if(dx > 0.1f)
+        {
+            scale.x = -0.3f;
+        }
+        else
+        {
+            scale.x = 0.3f;
+        }
+
+        transform.localScale = scale;
+	}
+}
--- Assets/Hayato/Script/EnemyController.cs
+++ Assets/Hayato/Script/EnemyController.cs
@@ -5,50 +5,66 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
     public GameObject target;
-    Vector3 prev; // １フレーム前の座標保持
     public float speed = 0.1f; // 移動量
     public float moveForce = 10.0f; // 移動量(Rigidbody用)
 
+    bool moveflag;
+
     void Start()
     {
-        prev = this.gameObject.transform.localPosition;
+        moveflag = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 enemyPos = this.gameObject.transform.localPosition;
+        Vector3 scale = transform.localScale;
+
         float vx = target.transform.position.x - enemyPos.x;
-        //float vy = target.transform.position.y - enemyPos.y;
-        float dx, dy, radian, value;
+        float vy = target.transform.position.y - enemyPos.y;
+        float dx, radian;
 
-        /**
-    　　　　 * ターゲットとのラジアン/ベクトル
-    　　　　 */
-        // Mathf.Atan2を使う場合
-        radian = Mathf.Atan2(0, vx); // ２つの座標から角度を計算
+        // Mathf.Atan2
+        radian = Mathf.Atan2(enemyPos.y, vx); // ２つの座標から角度を計算
         dx = Mathf.Cos(radian) * speed; // Sin,Cosを使用してその方向へ移動
-        //dy = Mathf.Sin(radian) * speed;
 
-        // Mathf.Atan2を使わない場合
-        //value = Mathf.Sqrt( (vx * vx) + (vy * vy) ); // ２つの座標からベクトル計算
-        //dx = (vx / value) * speed; // valueで割って正規化
-        //dy = (vy / value) * speed;
-
-        // 移動制御
-        if (Mathf.Abs(vx) < 0.1f)
+        if (moveflag == true)
         {
-            dx = 0f;
-        }
-        //if (Mathf.Abs(vy) < 0.1f)
-        //{
-        //    dy = 0f;
-        //}
+            // 移動制御
+            if (Mathf.Abs(vx) < 0.1f)
+            {
+                moveflag = false;
+            }
 
-        // 移動を反映
-        enemyPos.x += dx;
-        //enemyPos.y += dy;
+            // 移動を反映
+            enemyPos.x += dx;
+        }
+
+        if(moveflag == false)
+        {
+            // 移動を反映
+            enemyPos.x += speed;
+
+            if(Mathf.Abs(vy) < 0.1f)
+            {
+                moveflag = true;
+            }
+        }
+
+        if (dx >= 0)
+        {
+            // 右方向に移動中
+            scale.x = -0.3f; // そのまま（右向き）
+        }
+        else {
+            // 左方向に移動中
+            scale.x = 0.3f; // 反転する（左向き）
+        }
+        // 代入し直す
+        transform.localScale = scale;
 
         this.gameObject.transform.localPosition = enemyPos;
+
     }
 }
