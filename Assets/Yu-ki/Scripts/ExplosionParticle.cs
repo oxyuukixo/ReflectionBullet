be995e210d736c7afafe366f835a79e3d8208461
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-11-08 16:55:24 +0900

    no message

---
+++ Assets/Yu-ki/Scripts/ExplosionParticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionParticle : MonoBehaviour {
+
+    private float mLength;
+    private float mCur;
+
+    // Use this for initialization
+    void Start () {
+        Animator animOne = GetComponent<Animator>();
+        AnimatorStateInfo infAnim = animOne.GetCurrentAnimatorStateInfo(0);
+        mLength = infAnim.length;
+        mCur = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        mCur += Time.deltaTime;
+        if (mCur > mLength)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
