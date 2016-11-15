f0223dd78dd2f5d7c03b8f30fb76121170fa49bc
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-11-15 09:44:38 +0900

    no message

--- Assets/Yu-ki/Scripts/EnemyZoneManager.cs
+++ Assets/Yu-ki/Scripts/EnemyZoneManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyExtensionEditor : MonoBehaviour {
+public class EnemyZoneManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
