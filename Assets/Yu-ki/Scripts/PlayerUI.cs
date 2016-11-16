ddb4c816c30f7f9ee2641fbe20f167454fd54a37
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-11-16 15:54:27 +0900

    UI

--- Assets/Yu-ki/Scripts/PlayerUI.cs
+++ Assets/Yu-ki/Scripts/PlayerUI.cs
@@ -6,21 +6,42 @@ public class PlayerUI : MonoBehaviour {
 
     public GameObject m_Player;
 
-    private Player m_PlayerComp;
+    public Text m_BulletTypeText;
 
-    //使用するテキスト
-    private Text m_BulletType;
+    public RectTransform m_HPRedBar;
+    public RectTransform m_HPGreenBer;
+    public float m_RedBarSpeed = 0.3f;
+
+    private Player m_PlayerComp;
+    
+    //HPの最大値
+    private float m_MaxHP;
 
 	// Use this for initialization
 	void Start () {
 
         m_PlayerComp = m_Player.GetComponent<Player>();
+        m_MaxHP = m_PlayerComp.m_HP;
 
-        m_BulletType = transform.FindChild("BulletType").GetComponent<Text>();
+        m_PlayerComp.m_HP = 50;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        m_BulletType.text = m_PlayerComp.m_BulletType.ToString();
-	}
+
+        m_BulletTypeText.text = m_PlayerComp.m_BulletType.ToString();
+
+        m_HPGreenBer.localScale = new Vector3(m_PlayerComp.m_HP / m_MaxHP, m_HPGreenBer.localScale.y, m_HPGreenBer.localScale.z);
+
+        if(m_HPRedBar.localScale.x > m_HPGreenBer.localScale.x)
+        {
+            m_HPRedBar.localScale -= new Vector3(m_RedBarSpeed * Time.deltaTime,0,0);
+        }
+
+        if (m_HPRedBar.localScale.x < m_HPGreenBer.localScale.x)
+        {
+            m_HPRedBar.localScale = m_HPGreenBer.localScale;
+        }
+
+    }
 }
